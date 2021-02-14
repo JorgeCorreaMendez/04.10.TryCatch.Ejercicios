3df6c607f87c17fedc9840b093fa2601dabafba1
Jorge Correa <jorgecorreamen@gmail.com>
2021-02-14 12:11:58 +0000

    Gitignore y Ejercicio 1
    Añadido Ejercicio 1 completado y añadido el gitignore

---
+++ Ejercicio1.java
@@ -0,0 +1,50 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+
+        int dividiendo = 0;
+        int divisor = 0;
+        int cociente = 0;
+        int resto = 0;
+
+        boolean error = true;
+
+        while (error) {
+            System.out.print("Introduzca el dividiendo: ");
+            try {
+                dividiendo = leer.nextInt();
+                error = false;
+            } catch (InputMismatchException e) {
+                leer.nextLine();
+                System.out.println("Error, no has introducido un número en el dividiendo");
+            }
+        }
+
+        error = true;
+
+        while (error) {
+            System.out.print("Introduzca el divisor: ");
+            try {
+                divisor = leer.nextInt();
+                cociente += dividiendo / divisor;
+                resto += dividiendo % divisor;
+                error = false;
+
+                System.out.println("El resultado del cociente: " + cociente + " y el resultado del resto es: " + resto);
+
+            } catch (ArithmeticException e) {
+                System.out.println("Error, no se puede dividir entre 0 ");
+                leer.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println("Error, no has introducido un número en el divisor");
+                leer.nextLine();
+            }
+        }
+
+        leer.close();
+
+    }
+}
